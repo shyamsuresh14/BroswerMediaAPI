0e007bb935fc9b5f324e03f6bda7f1435cb081c9
Shyam Suresh <shyamsuresh1211@gmail.com>
2018-08-23 18:31:11 +0530

    Code for webcam api works fine

--- media/client/index.js
+++ media/client/index.js
@@ -1,25 +1,14 @@
-window.onload = function(){
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webKitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webKitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || 
     navigator.oGetUserMedia;
     
-    if(this.navigator.getUserMedia){
-        this.navigator.getUserMedia({video:true}, handleVideo, videoError);
-    }
-
-    function handleVideo(stream){
-        this.document.querySelector('#vid').src = window.URL.createObjectURL(stream);
-    }
-    
-    function videoError(){
-        alert('There is some problem');
-    }
-    /*var constraints = {audio:true, video: true};
-    navigator.mediaDevices.getUserMedia(constraints).then(function(){
-        var vid = document.querySelector('video');
-        vid.srcObject = MediaStream;
-        vid.play(); 
-    }).catch(function(err){
-        console.log("Error encountered" + err.message);
-    });*/
+if(navigator.getUserMedia){
+   navigator.getUserMedia({video:true}, handleVideo, videoError);
 }
 
+function handleVideo(stream){
+    document.querySelector('#vid').src = window.URL.createObjectURL(stream);
+}
+    
+function videoError(){
+    alert('There is some problem');
+}
